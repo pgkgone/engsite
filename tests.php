b0c55365d2a5abf5c46d4345ba17038e54638851
Megatron997 <Megatron997@mail.ru>
2019-04-11 17:17:18 +0300

    Для 1 части создан обработчик

--- tests.php
+++ tests.php
@@ -1,7 +1,7 @@
 <html>
 <head>
+    <div class="logo"> <img src="images/logo_small.png" height="100" width="100"></div>
     <link rel="stylesheet" href="css/bootstrap.min.css" >
-    <title>History of Physics - Part I</title>
     <style type="text/css">
         .main-menu-button{
             margin-top: 5px;
@@ -48,7 +48,7 @@ function createButtonsList(){
 EOT;
     $part = (int)$_GET['part'];
     $title= (int)$_GET['title'];
-    if($part>1){
+    if($part>0){
         $part=$part+1;
         $st="location.href = tests.php?title=".$title."&part=$part"."";
         echo '<input type="button" class="prev-button btn-primary" value="Prev part" onclick='. $st.'>';
@@ -64,10 +64,24 @@ EOT;
 </div>
 EOT;
 }
+
+function createFor1stPart(){
+    $partsAlpha = array("A", "B", "C", "D", "E","F","G","H");
+    $DB = new SQLite3("db.sqlite");
+    $result=(string)$DB->query("SELECT content FROM tests WHERE title = ". $_GET["title"]." AND part = ".   $_GET["part"]. "")->fetchArray()[0];
+    $arr=explode("|",$result);
+    for($i=0;$i<8;$i++){
+        echo "<h3 style='margin-left: 5vh'>$arr[$i]</h3>";
+    }
+    echo "<div style='display: inline-block; margin-left: 5vh'>";
+    for($i=0;$i<8;$i++){
+        echo '<a>'.$partsAlpha[$i].'</a><select id="selectItem" style="margin-right: 2vh"><option id="A">A</option><option id="B">B</option><option id="C">C</option><option id="D">D</option><option id="E">E</option><option id="F">F</option><option id="G">G</option><option id="H">H</option></select>';
+    }
+    echo "</div>";
+}
 ?>
 <body>
-<div class="logo"> <img src="images/logo_small.png" height="100" width="100"></div>
-<div class="content" align="center">
+<div class="content">
     <div class="header">
         <div class="Part 1">
             <h1 align="center"><span>Part I</span></h1>
@@ -81,13 +95,18 @@ EOT;
     </div>
     <?php
     createButtonsList();
-            $DB = new SQLite3("db.sqlite");
-            $result=$DB->query("SELECT content FROM tests WHERE title = ". $_GET["title"]." AND part = ".   $_GET["part"]. "");
-            $Processor = new ContentProcessor();
-            $content = $result->fetchArray()[0];
-            $Processor->InputProcessor($content);
-            echo $content;
-    echo '</div>';
+    echo "<title>History of Physics - Part </title>"; //!!!!!!!!!!!!!!! сделать надо
+    if((int)$_GET['part']==0){
+        createFor1stPart();
+    }else {
+        $DB = new SQLite3("db.sqlite");
+        $result = $DB->query("SELECT content FROM tests WHERE title = " . $_GET["title"] . " AND part = " . $_GET["part"] . "");
+        $Processor = new ContentProcessor();
+        $content = $result->fetchArray()[0];
+        $Processor->InputProcessor($content);
+        echo $content;
+        echo '</div>';
+    }
     createButtonsList();
     ?>
 </div>
