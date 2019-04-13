e3ff837e5006194c18715bd85ff2219a39a4ec07
Megatron997 <Megatron997@mail.ru>
2019-04-13 21:22:15 +0300

    1111

--- js/send-answer.js
+++ js/send-answer.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var pater=document.getElementsByClassName('main-text-only-one')[0].id;
         var title =  parseInt(pater/10);
         var part =pater%10;
-        var maxi=[6,7,7]
-        var arr = []
+        var maxi=[6,7,7];
+        var arr = [];
         if(part==0) {
-            var name = "selectItem"
+            var name = "selectItem";
             for (var i = 0; i < 6; i++) {
                 var elem = document.getElementById(name + i);
                 var answ = elem.options[elem.selectedIndex].text;
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var ajaxurl = 'checkAnswers.php',
             data =  {'title': title,'part':part,'arr':arr};
         $.post(ajaxurl, data, function (response) {
-            if(response==6){
+            if(response==maxi[part]){
                 swal("Good job!", "Your Score is "+response+"/"+maxi[part]+"!", "success");
             }else if(response==0){
                 swal("Oops!", "Your Score is "+response+"/"+maxi[part]+"!", "error");
