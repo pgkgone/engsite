fbe21be41ff57ce9a5bef1b6fcdc086a413f91fd
Megatron997 <Megatron997@mail.ru>
2019-04-13 18:40:59 +0300

    132

--- checkAnswers.php
+++ checkAnswers.php
@@ -3,7 +3,7 @@ function getScore($title,$part,$arr){
     $score=0;
     if($part==0){
         $DB = new SQLite3("db.sqlite");
-        $result=(string)$DB->query("SELECT answers FROM tests WHERE title= ". $_POST["title"]." AND part = ".   $_POST["part"]. "")->fetchArray()[0];
+        $result=(string)$DB->query("SELECT answers FROM tests WHERE title= ". $title." AND part = ".$part."")->fetchArray()[0];
         $ansArr=explode(",",$result);
         for($i=0;$i<6;$i++){
             if($ansArr[$i]==$arr[$i]) $score++;
