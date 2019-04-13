1e531b01bce2e5d6d8c7a8e918ba586255971e4a
Megatron997 <Megatron997@mail.ru>
2019-04-13 19:11:06 +0300

    132

--- js/send-answer.js
+++ js/send-answer.js
@@ -13,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 arr.push(answ);
             }
         }
-
         var ajaxurl = 'checkAnswers.php',
             data =  {'title': title,'part':part,'arr':arr};
         $.post(ajaxurl, data, function (response) {
